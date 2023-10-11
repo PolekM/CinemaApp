d7f401b86f3ea624f963a55c9aae9087269cf416
PolekM <marek.pawel.polek@gmail.com>
2023-10-11 17:18:42 +0200

    create simple controller to test future security

---
+++ src/main/java/com/cinema/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.cinema.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/admin")
+    public String adminTestController(){
+        return "Admin Controller";
+    }
+
+
+    @GetMapping("/user")
+    public String userTestController(){
+        return "User Controller";
+    }
+}
