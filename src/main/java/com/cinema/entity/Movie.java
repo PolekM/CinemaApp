321085451722f24d06930b041979b37a84facb18
PolekM <marek.pawel.polek@gmail.com>
2023-10-14 11:59:08 +0200

    create Movie repository & entity

---
+++ src/main/java/com/cinema/entity/Movie.java
@@ -0,0 +1,27 @@
+package com.cinema.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDate yearOfProduction;
+    @ManyToOne
+    @JoinColumn(name = "species_id")
+    private Species speciesId;
+}
