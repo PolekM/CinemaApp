eee86045246439314618ee6a1b43ac8c1c919af9
PolekM <marek.pawel.polek@gmail.com>
2023-10-15 14:52:27 +0200

    Adding repository annotation

--- src/main/java/com/cinema/repository/AppRoleRepository.java
+++ src/main/java/com/cinema/repository/AppRoleRepository.java
@@ -2,6 +2,8 @@ package com.cinema.repository;
 
 import com.cinema.entity.AppRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppRoleRepository extends JpaRepository<AppRole,Integer> {
 }
--- src/main/java/com/cinema/repository/SpeciesRepository.java
+++ src/main/java/com/cinema/repository/SpeciesRepository.java
@@ -2,6 +2,8 @@ package com.cinema.repository;
 
 import com.cinema.entity.Species;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpeciesRepository extends JpaRepository<Species,Integer> {
 }
