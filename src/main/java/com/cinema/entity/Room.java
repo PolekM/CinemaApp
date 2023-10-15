feab463b493f37942d7cc0eb305590c4f0958ffc
PolekM <marek.pawel.polek@gmail.com>
2023-10-15 14:53:43 +0200

    create entity Room & repository room

---
+++ src/main/java/com/cinema/entity/Room.java
@@ -0,0 +1,22 @@
+package com.cinema.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String roomName;
+    private Integer seatCount;
+}
