d1b61aec1bd0a926b5a75b41eee48eae3a4435ba
Marek Polek <marek.pawel.polek@gmail.com>
2024-02-29 15:50:11 +0100

    add url to dto

--- src/main/java/com/cinema/dto/seance/SeanceReadWithStarTimeListDto.java
+++ src/main/java/com/cinema/dto/seance/SeanceReadWithStarTimeListDto.java
@@ -13,12 +13,14 @@ public class SeanceReadWithStarTimeListDto {
     private Integer ticketCost;
     private String room;
     private String movie;
+    private String movieUlr;
     List<SeanceStartTime> seanceStartTimeList = new ArrayList<>();
 
     public SeanceReadWithStarTimeListDto(Seance seance){
         this.ticketCost = seance.getTicketCost();
         this.room =  seance.getRoom().getRoomName();
         this.movie = seance.getMovie().getTitle();
+        this.movieUlr = seance.getMovie().getMovieUrl();
         this.seanceStartTimeList.add(new SeanceStartTime(seance.getSeanceId(),seance.getStartTime()));
     }
 }
