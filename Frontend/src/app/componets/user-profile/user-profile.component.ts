32961e3e73492214561d9acfad62a71b822ee80f
Marek Polek <marek.pawel.polek@gmail.com>
2024-06-18 11:28:33 +0200

    add CORS for profile on backend and create method on Front to get User Data

--- Frontend/src/app/componets/user-profile/user-profile.component.ts
+++ Frontend/src/app/componets/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserReadDto } from '../../entity/UserReadDto';
+import { UserProfileService } from '../../service/user-profile.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
 
+  user: UserReadDto = {} as UserReadDto
+
+  constructor(private userProfileService: UserProfileService){
+
+  }
+  ngOnInit(): void {
+    this.getUserData()
+  }
+  getUserData(){
+    this.userProfileService.getUserData().subscribe(Response => this.user =Response)
+  }
+  
 }
--- Frontend/src/app/service/user-profile.service.ts
+++ Frontend/src/app/service/user-profile.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserReadDto } from '../entity/UserReadDto';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserProfileService {
 
-  constructor() { }
+ private url = 'http://localhost:8080/'
+
+  constructor(private http: HttpClient) { }
+
+  public getUserData(): Observable<UserReadDto>{
+    return this.http.get<UserReadDto>(`${this.url}`+'profile/data')
+  }
 }
